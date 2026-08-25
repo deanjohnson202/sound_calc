55ad0df63d3f3a82be84e69f8309662ab5077404
unknown <deanjohnson202@gmail.com>
2026-08-25 14:26:45 -0500

    program arithmetic

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ let firstNumber = null;
 let operator = null;
 let waitingForSecondNumber = false;
 
+function calculate(firstValue, secondValue, chosenOperator) {
+	if (chosenOperator === "+") {
+		return firstValue + secondValue;
+	}
+	if (chosenOperator === "-") {
+		return firstValue - secondValue;
+	}
+	if (chosenOperator === "*") {
+		return firstValue * secondValue;
+	}
+	if (chosenOperator === "/") {
+		if (secondValue === 0) {
+			return "Error";
+		}
+		return firstValue / secondValue;
+	}
+}
+
 buttons.forEach(function (button) {
 	button.addEventListener("click", function () {
 		const buttonText = button.textContent;
@@ -33,7 +51,26 @@ buttons.forEach(function (button) {
 				display.value = display.value + ".";
 			}
 		}
-	
+
+		if (buttonText === "=") {
+			if (
+			firstNumber !== null &&
+			operator !== null &&
+			!waitingForSecondNumber
+		) {
+			const secondNumber = Number(display.value);
+			const result = calculate(
+				firstNumber,
+				secondNumber,
+				operator
+				);
+
+				display.value = result;
+				firstNumber = null;
+				operator = null;
+				waitingForSecondNumber = true;
+			}
+		}
 
 		if (buttonText === "C") {
 			display.value = "0";
