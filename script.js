5f23448823b4a01d6ae2924ae91f6adeee119fcb
unknown <deanjohnson202@gmail.com>
2026-08-25 12:56:34 -0500

    Make number buttons update display

---
+++ script.js
@@ -0,0 +1,16 @@
+const discplay = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(function (button) {
+	button.addEventListener("click", function () {
+		const buttonText = button.textContent;
+
+		if (buttonText >= "0" && buttonText <= "9") {
+			if (display.value === "0") {
+				dislay.value = buttonText;
+			} else {
+				display.value = display.value + buttonText;
+			}
+		}
+	});
+});
