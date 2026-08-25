a935362776342baa3e40828127264cd5c2a8d3e3
unknown <deanjohnson202@gmail.com>
2026-08-25 14:06:29 -0500

    Button behavior

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
+let firstNumber = null;
+let operator = null;
+let waitingForSecondNumber = false;
 
 buttons.forEach(function (button) {
 	button.addEventListener("click", function () {
 		const buttonText = button.textContent;
 
 		if (buttonText >= "0" && buttonText <= "9") {
-			if (display.value === "0") {
+			if (waitingForSecondNumber) {
 				display.value = buttonText;
+				waitingForSecondNumber = false;
+			} else if (display.value === "0") {
+				display.value = buttonText;	
 			} else {
 				display.value = display.value + buttonText;
 			}
 		}
 
+		if (["/", "*", "-", "+"].includes(buttonText)) {
+			firstNumber = Number(display.value);
+			operator = buttonText;
+			waitingForSecondNumber = true;
+		}
+
 		if (buttonText === ".") {
-			if (!display.value.includes(".")) {
+			if (waitingForSecondNumber) {
+				display.value = "0.";
+				waitingForSecondNumber = false;
+			} else if (!display.value.includes(".")) {
 				display.value = display.value + ".";
 			}
 		}
+	
 
 		if (buttonText === "C") {
 			display.value = "0";
+			firstNumber = null;
+			operator = null;
+			waitingForSecondNumber = false;
 		}
 
 		if (buttonText === "Back") {
